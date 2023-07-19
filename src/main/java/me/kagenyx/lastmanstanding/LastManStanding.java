8c7074b31ae88ac61038d1e965d12b9e0c07ea91
Gonçalo Silva <goncalo.ribeiro.silva@live.com.pt>
2023-07-19 19:49:12 +0100

    Initial commit

---
+++ src/main/java/me/kagenyx/lastmanstanding/LastManStanding.java
@@ -0,0 +1,17 @@
+package me.kagenyx.lastmanstanding;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LastManStanding extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

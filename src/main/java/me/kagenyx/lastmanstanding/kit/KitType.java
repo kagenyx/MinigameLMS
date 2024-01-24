0d9e36204d0da350b7f7db192b03450990bae12a
Gonçalo Silva <goncalo.ribeiro.silva@live.com.pt>
2024-01-24 23:47:47 +0000

    Fixed all kits c:

--- src/main/java/me/kagenyx/lastmanstanding/kit/KitType.java
+++ src/main/java/me/kagenyx/lastmanstanding/kit/KitType.java
@@ -8,14 +8,14 @@ import org.bukkit.Material;
 import javax.naming.Name;
 
 public enum KitType {
-    ENDERBORN(Component.text( "Enderborn").color(NamedTextColor.DARK_PURPLE),Material.ENDER_EYE,Component.text("The End is Coming").color(NamedTextColor.GRAY)),
-    //MAGE(NamedTextColor.LIGHT_PURPLE + "Mage",Material.ENCHANTED_BOOK,NamedTextColor.GRAY + "In the Arcane lies true power for those who seek it!"),
-    //BEASTMASTER(NamedTextColor.GREEN + "Beastmaster",Material.BONE,NamedTextColor.GRAY + "The spirit of the hunt lives through me!"),
-    //SAMURAI(Component.text("Samurai", TextColor.fromHexString("#e890c3")).toString(),Material.IRON_SWORD,NamedTextColor.GRAY + "May honor preserve us."),
-    VIKING(Component.text("Viking").color(NamedTextColor.DARK_GRAY),Material.CHAINMAIL_HELMET,Component.text("The gods shall protect us.").color(NamedTextColor.GRAY));
-    //AGENT47(NamedTextColor.DARK_RED + "Agent 47",Material.POTION,NamedTextColor.GRAY+"Good job 47."),
-    //DREADWALKER(NamedTextColor.DARK_AQUA + "Dread Walker",Material.TRIDENT,NamedTextColor.GRAY + "They do not fear the depths of the waters; no, they fear us!"),
-    //NOOB(NamedTextColor.WHITE + "N00b",Material.STICK,NamedTextColor.GRAY + "How do I craft a Sponge?");
+    ENDERBORN(Component.text( "Enderborn").color(TextColor.fromHexString("#2f1163")),Material.ENDER_EYE,Component.text("The End is Coming").color(NamedTextColor.GRAY)),
+    MAGE( Component.text("Mage").color(NamedTextColor.LIGHT_PURPLE),Material.ENCHANTED_BOOK,Component.text("In the Arcane lies true power for those who seek it!").color(NamedTextColor.GRAY)),
+    BEASTMASTER(Component.text("Beastmaster").color(NamedTextColor.GREEN),Material.BONE,Component.text("The spirit of the hunt lives through me!").color(NamedTextColor.GRAY)),
+    SAMURAI(Component.text("Samurai").color(TextColor.fromHexString("#e890c3")),Material.IRON_SWORD,Component.text("May honor preserve us.").color(NamedTextColor.GRAY)),
+    VIKING(Component.text("Viking").color(NamedTextColor.DARK_GRAY),Material.CHAINMAIL_HELMET,Component.text("The gods shall protect us.").color(NamedTextColor.GRAY)),
+    AGENT47(Component.text("Agent 47").color(NamedTextColor.DARK_RED),Material.POTION,Component.text("Good job 47.").color(NamedTextColor.GRAY)),
+    DREADWALKER(Component.text("Dreadwalker").color(NamedTextColor.DARK_AQUA),Material.TRIDENT,Component.text("They do not fear the depths of the waters; no, they fear us!").color(NamedTextColor.GRAY)),
+    NOOB(Component.text("N00b").color(NamedTextColor.WHITE),Material.STICK,Component.text("How do I craft a Sponge?").color(NamedTextColor.GRAY));
 
     private Component text, desc;
     private Material material;
